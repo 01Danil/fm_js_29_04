bbdd094ad5d17d2b69a58bfdd5a97ea8c1e873c0
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-01 17:11:17 +0300

    Log primityve data types

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-//alert(77)
-/*
-многострочный комментарий
-*/
+console.log(12);
+console.log('Ilon Musk',1971);
+console.log(true);
+console.log(false);
+console.log(undefined);
+console.log(null);
