13b073f59ed6fa6517952bcf3fb4c820386e9f43
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-14 14:21:26 +0300

    Add an assignment from the lesson Decrement & Increment

--- index.js
+++ index.js
@@ -1,25 +1,64 @@
-/* Логическое сложение ИЛИ || */
-/* Возвращает первую правду или последнюю ложь */
-/* 1+0=1 1+1=2 0+1=1 0+0=0 */
+let number = 12;
+
+// number = number + 10;
+number +=10;
+
+number %=5;
+
+number **=3;
+
+number /= number;
+
+number *= 4;
+
+console.log(number);
 
 
-/* Логическое умножение И && */
-/* Возвращает последнюю правду или первую ложь */
-/* 1*0=0  0*1=0  0*0=0  1*1=1 */
 
-// let cond = 'cxcz' || 10;
-// console.log(cond);
+let nameUser = 'Elon';
+// nameUser = nameUser + +' Musk'
+nameUser += ' Musk';
+
+console.log(number, nameUser);
 
 
-let number = 21;
+// let a=2,b,c,res: Bad practic
 
-let min = 10;
-let max = 20;
 
-if( number<min || number>max ){
-	console.log('You win', number)
-}
+let a = 2;
+let b;
+let c;
+let res;
 
-// if( number>=min && number<=max){
-// 	console.log('You win!', number)
-// };
+
+let number1 = 45;
+let number2 = 12;
+
+console.log(
+	number1 + number2, 
+	number1 - number2, 
+	number1 * number2, 
+	number1 / number2, 
+	number1 % number2
+	);
+
+let number3 = 101;
+let resultBoolean = true;
+let programmingLanguage = 'Java';
+let name = 'Elon Musk';
+let value = null;
+
+number1 = number1++;
+number1 = ++number1;
+console.log(number1);
+number1 = number1--;
+number1 = --number1;
+console.log(number1);
+
+let numberOfTheThirdTask = 23;
+numberOfTheThirdTask += 11;
+numberOfTheThirdTask /=2;
+numberOfTheThirdTask -=1;
+numberOfTheThirdTask *=11;
+numberOfTheThirdTask += 1;
+numberOfTheThirdTask %=5;
