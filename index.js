a5e31f98983506a8280b7020d851a4968e864520
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-14 13:25:18 +0300

    Add logic operators: && and ||

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-// let num = 10;
+/* Логическое сложение ИЛИ || */
+/* Возвращает первую правду или последнюю ложь */
+/* 1+0=1 1+1=2 0+1=1 0+0=0 */
 
-// console.log(num<=10);
 
-console.log('test'>'Z')
+/* Логическое умножение И && */
+/* Возвращает последнюю правду или первую ложь */
+/* 1*0=0  0*1=0  0*0=0  1*1=1 */
+
+// let cond = 'cxcz' || 10;
+// console.log(cond);
+
+
+let number = 21;
+
+let min = 10;
+let max = 20;
+
+if( number<min || number>max ){
+	console.log('You win', number)
+}
+
+// if( number>=min && number<=max){
+// 	console.log('You win!', number)
+// };
