f32684a6f5ea41ef92b87b35d2b562158f37e318
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-15 15:21:34 +0300

    Refactor calkSummaTwoNumberAtUser

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const calkSummaTwoNumberReqAtUser = function () {
-  let number1 = prompt("enter number 1", "10");
-  let number2 = prompt("enter number 2", "20");
-	//ошибка: если пустая строка, либо нажали отмена, либо ввели что-то, что не цифры 
-		const error = number1 === '' || number2 === '' || number1 === null || number1 === null || isNaN(Number(number1)) || isNaN(Number(number2)); 
-		if(error){
-		//true
-		//обработка ошибки
-		return 'error' // exit
-	}
-	// let summa = Number(number1) + Number(number2);
-	// return summa;
-		return Number(number1) + Number(number2); 
+
+const number1 = prompt("enter number 1", "10");
+const number2 = prompt("enter number 2", "20");
+/**
+ * Discription function 
+ * @param {null|string} num 
+ * @returns {boolean}
+ */
+const isErrorInputNumber = function(num){
+	return num === '' || num === null || isNaN(Number(num));
+}
+/**
+ * Return summa two number.
+ * @param {number} number1 
+ * @param {number} number2 
+ * @returns {number}
+ */	
+const calkSummaTwoNumber = function (number1, number2) {
+	return number1 + number2; 
+};
+const check = isErrorInputNumber(number1) || isErrorInputNumber(number2);
+if(check){
+	console.log('not possible calc summa')
+}else{
+	const summaUser = calkSummaTwoNumber(Number(number1), Number(number2));
+	console.log(summaUser);
 };
 
-let summa = calkSummaTwoNumberReqAtUser()
-console.log(summa);
+
