7486ec44733ecb5a3f55db7427939cde24e2b432
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-19 18:06:46 +0300

    Exemple ternary operator

--- index.js
+++ index.js
@@ -1,39 +1,13 @@
-// const triangleOnThreeSides = function (firstSide = 3, secondSide = 4, thirdSide = 5) {
-// 	return firstSide + secondSide > thirdSide && secondSide + thirdSide > firstSide && firstSide + thirdSide > secondSide
-// };
-// console.log(triangleOnThreeSides(1,1,10)); 
+// Conditional (ternary) operator
 
-// const calkVolumeSfera = function (radius = 100, PI = 3.14) {
-// 	if ( radius <=0 ) {
-// 		return NaN; // false, null
-// 	}
-// 	return 4*PI*(radius**3)/3;
-// };
-// console.log(calkVolumeSfera(23, 3.1415926));
-
-const userInput = prompt ('Enter something 1');
-const ValueToUser = function () {
-	if (userInput === null || userInput === '') {
-		return 'Повторите ввод';
-	}
-	return 'Ты ввел: ' + userInput;
+//true | false
+const isEven = function (number = 0) {
+	return number % 2 === 0;
 };
-console.log(ValueToUser());
+console.log(isEven());
 
-const f = function () {
-	const input = prompt ('Input something 2');
-	if (input === null || input === '') {
-		return 'Repeat input';
-	}
-	return 'You enter: ' + input;
+// 'even' | 'odd'
+const returnEvenOrOdd = function (number = 0) {
+	return ( number % 2 === 0 ) ? 'even' : 'odd';
 };
-console.log(f());
-
-const userValueReturn = function () {
-	const inputValue = prompt ('Enter something 3')
-	if (inputValue) {
-		return 'You enter ' + inputValue;
-	}
-	return 'Repeat input';
-};
-console.log(userValueReturn());
+console.log(returnEvenOrOdd());
