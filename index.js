265541a67f328b3f0412780373020ec347f5e583
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-19 19:01:19 +0300

    Used Conditional (ternary) operator

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 // Conditional (ternary) operator
 
 //true | false
-const isEven = function (number = 0) {
-	return number % 2 === 0;
-};
-console.log(isEven());
+// const isEven = function (number = 0) {
+// 	return number % 2 === 0;
+// };
+// console.log(isEven());
 
 // 'even' | 'odd'
-const returnEvenOrOdd = function (number = 0) {
-	return ( number % 2 === 0 ) ? 'even' : 'odd';
-};
-console.log(returnEvenOrOdd());
+// const returnEvenOrOdd = function (number = 0) {
+// 	return ( number % 2 === 0 ) ? 'even' : 'odd';
+// };
+// console.log(returnEvenOrOdd());
+
+// const userAge = function (age) {
+// 	return age >=18 ? 'adult' : 'not adult'
+// };
+// console.log(userAge());
+
+
+// const userAge = function () {
+// 	const checkUserAge = prompt ('Enter you age');
+// 	return checkUserAge >=18 ? 'adult' : 'not adult'
+// };
+// console.log(userAge());
+
+const summa = 123456;
+const LIMIT_SUMMA = 10000;
+
+const sale = summa > LIMIT_SUMMA ? 0.1 : 0.02;
+
+// const classInput = isValid ? 'valid' : 'invalid';
+
