06b40c99a48420450457c91b66cb62126ccd8ab8
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-19 14:52:33 +0300

    Add calkVolumeSfera

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-/**
- * 
- * @param {Number} firstSide 
- * @param {Number} secondSide 
- * @param {Number} thirdSide 
- * @returns {Boolean}
- */
-const triangleOnThreeSides = function (firstSide = 3, secondSide = 4, thirdSide = 5) {
-	return firstSide + secondSide > thirdSide && secondSide + thirdSide > firstSide && firstSide + thirdSide > secondSide
+// const triangleOnThreeSides = function (firstSide = 3, secondSide = 4, thirdSide = 5) {
+// 	return firstSide + secondSide > thirdSide && secondSide + thirdSide > firstSide && firstSide + thirdSide > secondSide
+// };
+// console.log(triangleOnThreeSides(1,1,10)); 
+
+const calkVolumeSfera = function (radius = 100, PI = 3.14) {
+	if ( radius <=0 ) {
+		return NaN; // false, null
+	}
+	return 4*PI*(radius**3)/3;
 };
-console.log(triangleOnThreeSides(1,1,10)); 
+console.log(calkVolumeSfera(23, 3.1415926));
