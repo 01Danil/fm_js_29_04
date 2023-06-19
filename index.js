bcd38fabc8900ac4f8bce6ca618bda6efbef98c1
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-19 12:51:58 +0300

    Add isAliquotAndBigger

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 
-/** Age check
+const number1 = prompt ('enter number', '');
+const number2 = prompt ('enter number', '');
+/**
  * 
- * @param {number} age 
+ * @param {number} number1 
+ * @param {number} number2 
  * @returns {boolean}
  */
-const isAdult = function (age=0, ADULT=18){
-	 return age >= ADULT
-	// if (age < 18){
-	// return false
-	// }
-	// return true
+const numberCheck = function (number1 = 10, number2 = 5){
+	const result = number1 % number2;
+	return result > 0 || result < 0;
 };
-const result = isAdult(18, 21);
-console.log(result);
+const result = numberCheck();
+console.log (result);
+
+
+/**
+ * 
+ * @param {number} num 
+ * @returns {boolean}
+ */
+const numberCheck2 = function (num = 0, first = 20, second = 7){
+	return num > first && num % second === 0; 
+}
