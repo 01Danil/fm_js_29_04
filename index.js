80e14e8e83f68a67d87601892441fd8d0dc78356
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-19 16:22:05 +0300

    Example function > prompt

--- index.js
+++ index.js
@@ -3,10 +3,28 @@
 // };
 // console.log(triangleOnThreeSides(1,1,10)); 
 
-const calkVolumeSfera = function (radius = 100, PI = 3.14) {
-	if ( radius <=0 ) {
-		return NaN; // false, null
+// const calkVolumeSfera = function (radius = 100, PI = 3.14) {
+// 	if ( radius <=0 ) {
+// 		return NaN; // false, null
+// 	}
+// 	return 4*PI*(radius**3)/3;
+// };
+// console.log(calkVolumeSfera(23, 3.1415926));
+
+const userInput = prompt ('Enter something');
+const ValueToUser = function () {
+	if (userInput === null || userInput === '') {
+		return 'Повторите ввод';
 	}
-	return 4*PI*(radius**3)/3;
+	return 'Ты ввел: ' + userInput;
 };
-console.log(calkVolumeSfera(23, 3.1415926));
+console.log(ValueToUser());
+
+const f = function () {
+	const input = prompt ('Input something');
+	if (input === null || input === '') {
+		return 'Repeat input'
+	}
+	return 'You enter: ' + input;
+};
+console.log(f());
