c08e7b08e3f6fea96d71013651531f44d3632a91
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-21 20:28:33 +0300

    Example password while(true)

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+let countTry = 0;
 while (true) {
-	console.log(2);
-	if (true) {
-		break;
+  const userInput = prompt("enter password", '***');
+  countTry++;
+	alert('try = ' + countTry);
+	if (countTry >= 3) {
+		alert('you used all your efforts')
+		break
 	}
+  if (userInput === CORRECT_PASSWORD) {
+    alert("welcome");
+    break;
+  }
 }
 
-do {
-	console.log(2);
-	if (true) {
-		break;
-	}
-} while (true);
