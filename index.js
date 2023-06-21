da5eb0c565afba271347613dcef5c70938e1e05b
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-21 19:43:34 +0300

    Example do\while

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-// let count = 2;
-//  debugger
-// while (count > 0) {
-// 	console.log(count); //
-// 	count-- ;
-// };
-
-debugger;
-let count = 0;
 while (true) {
-  count++;
-  if (count > 3) {
-    break;
-  }
-
-  console.log(count);
+	console.log(2);
+	if (true) {
+		break;
+	}
 }
+
+do {
+	console.log(2);
+	if (true) {
+		break;
+	}
+} while (true);
