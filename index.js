2f1c223edd167ae82a688c6e6d05a15e48b0ad14
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-22 13:04:34 +0300

    Example for loop

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-let countTry = 0;
-while (countTry < MAX_TRY) {
-  const userInput = prompt("enter password", '***');
-  countTry++;
-	alert('try = ' + countTry);
+
+// debugger
+// let countTry = 0; 																		// i
+// while (countTry < MAX_TRY) { 												// i < MAX_TRY
+//   const userInput = prompt("enter password", '***');
+//   countTry++; 																				// i++
+// 	alert('try = ' + countTry);
+//   if (userInput === CORRECT_PASSWORD) {
+//     alert("welcome");
+//     break;
+//   }
+// }
+
+
+
+for (let i = 1; i <= MAX_TRY; i++) {
+  const userInput = prompt("enter password", "***");
+  alert("try = " + i);
   if (userInput === CORRECT_PASSWORD) {
     alert("welcome");
     break;
   }
 }
-
