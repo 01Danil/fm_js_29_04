2639e5410d81bb8d933c34b7717702d2804d6826
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-06-23 15:11:00 +0300

    Example constructor function

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
 "use strict"; // es2015
 
-const cat1 = {
-  //literal
-  name: "Muha",
-  isMale: false,
-  age: 3,
-  breed: "street",
-  color: "grey",
-  say: function () {
-    return "meow";
-  },
-  run: function () {
-    return "i'm runing";
-  },
-};
-// alert (cat1);
-console.log(cat1);
-console.log(cat1.age);
-cat1.age++;
-console.log(cat1.say());
-console.log(cat1.run());
-cat1.colorEye = "green";
-console.log(cat1);
-//cat1.colorEye = undefined;
-delete cat1.breed;
-console.log(cat1);
+// function Dog(name, IsMale) {
+//   this.name = name;
+//   this.IsMale = IsMale;
+//   this.say = function (voice = 'Wof') {
+//     return voice;
+//   };
+// }
 
+// const testDog = new Dog();
+// console.log(testDog);
+// const dog1 = new Dog("Oskar", true);
+// console.log(dog1);
+// console.log(dog1.say());
 
+function User(name, sname, age) {
+  this.name = name;
+  this.sname = sname;
+  this.age = age;
+  this.getFullName = function (fullName = name + "" + sname) {
+    return fullName;
+  };
+}
 
-const dog1 = {
-  name: "Lika",
-  isMale: false,
-  say: function () {
-    return "Gav";
-  },
-};
-console.log(dog1);
-
-const dog2 = {
-  name: "Charly",
-  isMale: true,
-  say: function () {
-    return "Af";
-  },
-};
-console.log(dog2);
+const user1 = new User("Tom ", "Kruz", 18);
+console.log(user1);
+console.log(user1.getFullName());
+const user2 = new User("David ", "Beckhem", 18);
+console.log(user2);
+console.log(user2.getFullName());
+const user3 = new User("Danil ", "Kucheryavko", 18);
+console.log(user3);
+console.log(user3.getFullName());
