9dd77236917883057eee2242fa00b223553fe5a2
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-07-11 16:23:35 +0300

    Create MyArrayPrototype

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 "use strict";
-
+// реализовать аналог встроенного объекта Array
+// конструктор прототип-родитель с логикой с методами
+function MyArrayPrototype() {
+  this.push = function (value) {
+    this[this.length] = value;
+   // this.length;
+    return ++this.length;
+  };
+  this.pop = function () {
+    if (this.length === 0) {
+      return;
+    }
+    const lastItem = this[this.length - 1];
+    delete this[--this.length];
+   // this.length;
+    return lastItem;
+  };
+}
+// конструктор объекта с данными
 function MyArray() {
   this.length = 0;
-	this.push = function(value){
-		this[this.length] = value;
-		this.length++;
-		return this.length;
-	}
 }
+
+MyArray.prototype = new MyArrayPrototype();
 
 const myArray1 = new MyArray();
 const myArray2 = new MyArray();
 console.log(myArray1.length);
 console.log(myArray1.push(8));
-console.log(myArray1.push(7));
 console.log(myArray1);
-console.log(myArray1.push===myArray2.push);
+console.log(myArray1.push === myArray2.push);
 
-const array1 = new Array();
-const array2 = new Array();
-console.log(array1.length);
-console.log(array1.push(8));
-console.log(array1===array2);
-console.log(array1.push===array2.push);
+// const array1 = new Array();
+// const array2 = new Array();
+// console.log(array1);
+// console.log(array1.push(8));
+// console.log(array1 === array2);
+// console.log(array1.push === array2.push);
