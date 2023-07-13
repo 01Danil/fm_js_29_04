67a8bde61a79b8bace2e5585c72d5f656c2adba5
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-07-13 14:06:36 +0300

    Example rabbit

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 "use strict";
 
-const toString = function () {
-  return `${this.name} ${this.sname}`;
-};
-const animal = {
-	// name: "aninimus",
-  eat: function () {
-    return this.name + " is eating";
-  },
-};
-const man = {
-  countFingers: 5,
-	name: "human",
-  read: function () {
-    return this.name + " is reading";
-  },
-};
+// const toString = function () {
+//   return `${this.name} ${this.sname}`;
+// };
+// const animal = {
+//   // name: "aninimus",
+//   eat: function () {
+//     return this.name + " is eating";
+//   },
+// };
+// const man = {
+//   countFingers: 5,
+//   name: "human",
+//   read: function () {
+//     return this.name + " is reading";
+//   },
+// };
 
-const student = {
-  id: 1,
-  name: "Elon",
-  sname: "Musk",
-  age: 50,
-  toString, // toString: toString,
-};
-man.__proto__ = animal;
-student.__proto__ = man;
-// student.__proto__ = animal; // перезапишет
-console.log(student);
-console.log(student.read());
-console.log(man.eat());
+// const student = {
+//   id: 1,
+//   name: "Elon",
+//   sname: "Musk",
+//   age: 50,
+//   toString, // toString: toString,
+// };
+// man.__proto__ = animal;
+// student.__proto__ = man;
+// // student.__proto__ = animal; // перезапишет
+// console.log(student);
+// console.log(student.read());
+// console.log(man.eat());
 
 // const student2 = {
 //   id: 2,
@@ -43,3 +43,31 @@ console.log(man.eat());
 // console.log(student2);
 // console.log(student2.toString());
 // console.log(student2.toString === student.toString);
+
+const rabbit = {
+  countPaw: 4,
+  jump: function () {
+    return "i`m jumping";
+  },
+};
+
+const rabbitPet = {
+  name: "Nelly",
+  pet: function () {
+    return "pet " + this.name;
+  },
+};
+
+const rabbitMagic = {
+  say: function () {
+    return this.say;
+  },
+};
+rabbitPet.__proto__ = rabbit;
+rabbitMagic.__proto__ = rabbitPet;
+console.log(rabbit);
+console.log(rabbit.countPaw);
+console.log(rabbit.jump());
+console.log(rabbitPet.name);
+console.log(rabbitPet.pet());
+console.log(rabbitMagic.say("yes"));
