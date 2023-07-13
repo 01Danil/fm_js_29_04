7d6b3947b811bb2c80453a0b7ecbdbd889f18db9
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-07-13 14:58:00 +0300

    Example PrototypePrototype

--- index.js
+++ index.js
@@ -1,73 +1,32 @@
 "use strict";
-
-// const toString = function () {
-//   return `${this.name} ${this.sname}`;
-// };
-// const animal = {
-//   // name: "aninimus",
-//   eat: function () {
-//     return this.name + " is eating";
-//   },
-// };
-// const man = {
-//   countFingers: 5,
-//   name: "human",
-//   read: function () {
-//     return this.name + " is reading";
-//   },
-// };
-
-// const student = {
-//   id: 1,
-//   name: "Elon",
-//   sname: "Musk",
-//   age: 50,
-//   toString, // toString: toString,
-// };
-// man.__proto__ = animal;
-// student.__proto__ = man;
-// // student.__proto__ = animal; // перезапишет
-// console.log(student);
-// console.log(student.read());
-// console.log(man.eat());
-
-// const student2 = {
-//   id: 2,
-//   name: "Bob",
-//   sname: "Rob",
-//   age: 33,
-//   toString: toString,
-// };
-
-// console.log(student.toQwerty); // undefined
-// console.log(student2);
-// console.log(student2.toString());
-// console.log(student2.toString === student.toString);
-
-const rabbit = {
-  countPaw: 4,
-  jump: function () {
-    return "i`m jumping";
-  },
+function StudentPrototypePrototype () {
+	this.eat = function () {
+    return this.name + " is eating";
+  };
 };
 
-const rabbitPet = {
-  name: "Nelly",
-  pet: function () {
-    return "pet " + this.name;
-  },
-};
+function StudentPrototype() {
+  this.toString = function () {
+    return `${this.name} ${this.sname}`;
+  };
+  
+  this.read = function () {
+    return this.name + " is reading";
+  };
+}
+StudentPrototype.prototype = new StudentPrototypePrototype();
+function Student(name, sname, age = 22) {
+  if (!new.target) {
+    return new Student(name, sname, age);
+  }
+  this.name = name;
+  this.sname = sname;
+  this.age = age;
+}
+Student.prototype = new StudentPrototype();
 
-const rabbitMagic = {
-  say: function () {
-    return this.say;
-  },
-};
-rabbitPet.__proto__ = rabbit;
-rabbitMagic.__proto__ = rabbitPet;
-console.log(rabbit);
-console.log(rabbit.countPaw);
-console.log(rabbit.jump());
-console.log(rabbitPet.name);
-console.log(rabbitPet.pet());
-console.log(rabbitMagic.say("yes"));
+const student = new Student("Elon", "Musk", 50);
+console.log(student);
+console.log(student.toString());
+console.log(student.read());
+console.log(student.eat());
