02f10d0266c0eba291a01e28b59f016863e929a6
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-07-15 14:13:09 +0300

    Example stairs up/down

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
 "use strict";
-function StudentPrototypePrototype () {
-	this.eat = function () {
-    return this.name + " is eating";
-  };
-};
+// function StudentPrototype() {
+//   this.toString = function () {
+//     return `${this.name} ${this.sname}`;
+//   };
+//   this.read = function () {
+//     return this.name + " is reading";
+//   };
+//   this.eat = function () {
+//     return this.name + " is eating";
+//   };
+// }
+// function Student(name, sname, age = 22) {
+//   this.name = name;
+//   this.sname = sname;
+//   this.age = age;
+// }
+// Student.prototype = new StudentPrototype();
 
-function StudentPrototype() {
-  this.toString = function () {
-    return `${this.name} ${this.sname}`;
+// const student = new Student("Elon", "Musk", 50);
+// console.log(student);
+
+function LadderProto() {
+  this.up = function () {
+    this.step++;
+		return this;
   };
-  
-  this.read = function () {
-    return this.name + " is reading";
+  this.down = function () {
+    this.step--;
+		return this;
+  };
+  this.showStep = function () {
+    return this.step;
   };
 }
-StudentPrototype.prototype = new StudentPrototypePrototype();
-function Student(name, sname, age = 22) {
-  if (!new.target) {
-    return new Student(name, sname, age);
-  }
-  this.name = name;
-  this.sname = sname;
-  this.age = age;
+function Ladder() {
+  this.step = 0;
 }
-Student.prototype = new StudentPrototype();
 
-const student = new Student("Elon", "Musk", 50);
-console.log(student);
-console.log(student.toString());
-console.log(student.read());
-console.log(student.eat());
+Ladder.prototype = new LadderProto();
+
+const stairs = new Ladder();
+console.log(stairs.showStep()); // 0
+console.log(stairs.up().up().up().down().up().showStep());
+// ladder.up();
+// console.log(ladder.showStep()); // 1
+// ladder.up();
+// console.log(ladder.showStep()); // 2
+// ladder.up();
+// console.log(ladder.showStep()); // 3
+// ladder.down();
+// console.log(ladder.showStep()); // 2
