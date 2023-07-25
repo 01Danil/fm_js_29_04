ea5fc245d29385737afbd2e4be321d5ed3edc7d1
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-07-25 16:00:45 +0300

    Add 3 variant function  =>

--- index.js
+++ index.js
@@ -1,72 +1,17 @@
-"use strict";
-
-function User(fname, sname, age, isMale, email, isSubscribing = false) {
-  this.fname = fname;
-  this.sname = sname;
-  this.age = age;
-  this.isMale = isMale;
-  this.email = email;
-  this.isSubscribing = isSubscribing;
+"use strict"; // ES6 ES2015
+// декларация
+function test1() {
+  console.log(this);
 }
-function UserPrototype() {
-  this.getFullName = function () {
-    return `${this.fname} ${this.sname}`;
-  };
-  this.filter = function (callback) {};
-}
-User.prototype = new UserPrototype();
-const createRandomUsers = function (amount = 1) {
-  const db = [];
-  for (let i = 0; i < amount; i++) {
-    const user = new User(
-      `Name${i}`,
-      `Sname${i}`,
-      Math.floor(Math.random() * (80 - 20)) + 20, // [20...80}
-      Math.random() > 0.5, // true
-      `email${i}@gmail.com`
-    );
-    db.push(user);
-  }
-  return db;
+//выражение
+const test2 = function () {
+  console.log(this);
+};
+//стрелка
+const test3 = () => {
+  console.log(this);
 };
 
-const users = createRandomUsers(50);
-console.table(users);
-
-/* получить массив полных имен пользователей используя метод map */
-const newUser = users.map(function (user) {
-  return user.getFullName();
-});
-console.table(newUser);
-
-/* получить массив пользователей, старше 65, используя filter */
-const newOldUser = users.filter(function (user) {
-  return user.age > OLD_AGE; // true | false
-});
-console.table(newOldUser);
-
-/* получить массив пользователей female, используя filter */
-const newFemaleUsers = users.filter(function (user) {
-  return user.isMale === false;
-});
-console.table(newFemaleUsers);
-
-/* зарандомить подписку у пользователя 
-используя forEach */
-users.forEach(function (user) {
-  user.isSubscribing = Math.random() > 0.5; // true || false
-});
-console.table(users);
-
-// получить массив бабушек с подпиской
-const oldFemaleUsersWithSubscribing = users
-  .filter(function (user) {
-    return user.isMale === false;
-  })
-  .filter(function (user) {
-    return user.age > OLD_AGE;
-  })
-  .filter(function (user) {
-		return user.isSubscribing;
-  });
-console.table(oldFemaleUsersWithSubscribing);
+test1();
+test2();
+test3();
