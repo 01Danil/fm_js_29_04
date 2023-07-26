7fcd84f1288d03de0ce1639b1b2ba3b522daeb6f
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-07-26 13:12:56 +0300

    Add bind()

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 "use strict";
 
+function show(header) {
+  console.log(this);
+  console.log(header, "|", this.title);
+}
+
 const site = {
-  title: "Green site", ///
+  title: "Green site",
   headers: ["title 1", "About us", "Portfolio"],
   showHeaders() {
-    // console.log(this)
-    this.headers.forEach((header) => {
-      // console.log(this)
-      console.log(header, "|", this.title);
-
-      const test = () => {
-        console.log(this);
-      };
-      test();
-    });
+    console.log(this);
+		const test = show.bind(this)
+		console.dir(test)
+    this.headers.forEach(test);
   },
 };
 
