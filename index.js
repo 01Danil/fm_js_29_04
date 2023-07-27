868f8dccd2c91c8375f45df173dc19cdeada8e20
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-07-27 19:04:43 +0300

    Beta version stringToJaden

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 "use strict";
 
-const num = new Number(10);
-console.log(num);
+//const userName = new String("Elon");
+//const userName = "Elon".toUpperCase();
 
-const num2 = Number((123.123123).toFixed(2));
-console.log(num2);
+// let userName = "Elon";
+// userName = userName.toUpperCase();
 
-let number = 45.7879;
-number = Number(number.toFixed(1));
-console.log(number);
+/**
+написать функцию, которая преобразует строку к Jaden case - каждая слова с большой буквы 'To Be Or Not To Be'
+1) Разбить строку на слова - split => array
+2) Перебираем массив с каждым словом: 
+- найти первую букву и сделать большой => [0] toUpperCase
+- остальные буквы сделать маленькими => substring(1) to LowerCase
+- склеить первую букву и оставшиеся слово (+)
+3) Массив превратить назад в строку => join('')
+4) Вернуть полученную строку
+ */
+function stringToUpper(string, separeted = " ") {
+  return string
+    .split(separeted) // 1) Разбить строку на слова - split => array
+    .map(function (element) {
+      // 2) Перебираем массив ...
+      // - найти первую букву и сделать большой => [0] toUpperCase
+      // склеить +
+      // остальные буквы сделать маленькими
+      return element[0].toUpperCase() + element.substring(1).toLowerCase();
+    }) // новый массив из правильных слов
+    .join(separeted); // 3) Массив превратить назад в строку => join('')
+}
+console.log(stringToUpper("tO be OR Not TO bE"));
+
+const str = "tO be OR Not TO bE";
+function stringToJadenCase(str) {
+  let words = str.split(" ");
+  let stringFinal = "";
+
+  for (let i = 0; i < words.length; i++) {
+    let first = words[i].substring(0, 1).toUpperCase();
+    let leftovers = words[i].substring(1).toLowerCase();
+    stringFinal += first + leftovers + " ";
+  }
+  return stringFinal;
+}
+console.log(stringToJadenCase(str));
+
+function splitString(string, separeted) {
+  const arrayOfString = string.split(separeted);
+  for (let i = 0; i < arrayOfString.length; i++) {
+    arrayOfString[i] =
+      arrayOfString[i].substring(0, 1).toUpperCase() +
+      arrayOfString[i].substring(1).toLowerCase();
+  }
+  return arrayOfString.join(separeted);
+}
+const tempestString = "sadsadsa dsadas das dss adsa sa sa sad asd";
+const space = " ";
+console.log(splitString(tempestString, space));
