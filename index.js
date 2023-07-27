2ce931c8a1326279b9b7ba7f07f16974875f18c8
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-07-27 12:09:43 +0300

    "use strict" & this

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 "use strict";
-
-// recursion
-// рекурсия небезопасна !!!
-// первым описываем базовый случай !!!
-
-function recursion(num) {
-  if (num === 0 || num < 0) {
-    return;
-  }
-  console.log(num);
-  recursion(num - 1);
-}
-
-recursion(4);
-
-console.log("Info!!!");
+// когда мы подключаем строгий режим - "use strict", то функции, которые мы объявляем обычными способами с помощью декларации, либо когда функция объявленна с помощью выражения они перестают видеть окружение в котором находятся и у них появляеться свой "this", функции теперь не отталкиваются от Window, они не видят Window, у них формируется собственное окружение.
 
 /*
-4! = 4 * 3!
-3! = 3 * 2!
-2! = 2 * 1!
-1! = 1
+напишите функцию стрелка, которая принимает два числа и возвращает большее из двух чисел
+n1>n2 ? n1 : n2
 */
 
-const factorial = (n) => {
-  if (n === 1 || n === 0) {
-    return 1;
+const twoNumber = (num1, num2) => {
+  if (num1 > num2) {
+    return num1;
+  } else if (num1 < num2) {
+    return num2;
   }
-	return n*factorial(n-1)
 };
-console.log(factorial(3))
+console.log(twoNumber(4, 6));
+
+// const getMaxFromTwoNumber = (n1, n2) => {return n1 > n2 ? n1 : n2;};
+
+const getMaxFromTwoNumber = (n1, n2) => (n1 > n2 ? n1 : n2);
+console.log(getMaxFromTwoNumber(8, 6));
+
+const showArgs = (a, b, ...c) => {
+  console.log(c);
+  c.forEach((item) => {
+    console.log(this);
+  });
+};
+
+showArgs(1, 2, 3, 4, 5); // undefined
+
+const obj = {
+  prop: 12,
+  showArgs,
+};
+obj.showArgs(1, 1, 1, 1, 1); // {object}
