1a6abcc0b61b7a4da2f23700bbc59cbcb8a69a8e
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-07-28 15:58:09 +0300

    Example array (forEach, map, filter, reduce, find, findIndex)

--- index.js
+++ index.js
@@ -1,58 +1,74 @@
 "use strict";
 
-//const userName = new String("Elon");
-//const userName = "Elon".toUpperCase();
+const people = [
+  { name: "Vladlen", age: 25, budget: 40000 },
+  { name: "Elena", age: 17, budget: 3400 },
+  { name: "Igor", age: 49, budget: 50000 },
+  { name: "Mihail", age: 15, budget: 1800 },
+  { name: "Vasilisa", age: 24, budget: 25000 },
+  { name: "Victoria", age: 38, budget: 2300 },
+];
+console.log(people.length);
 
-// let userName = "Elon";
-// userName = userName.toUpperCase();
+// for (let i = 0; i < people.length; i++) {
+//   console.log(people[i]);
+// }
 
-/**
-написать функцию, которая преобразует строку к Jaden case - каждая слова с большой буквы 'To Be Or Not To Be'
-1) Разбить строку на слова - split => array
-2) Перебираем массив с каждым словом: 
-- найти первую букву и сделать большой => [0] toUpperCase
-- остальные буквы сделать маленькими => substring(1) to LowerCase
-- склеить первую букву и оставшиеся слово (+)
-3) Массив превратить назад в строку => join('')
-4) Вернуть полученную строку
- */
-function stringToUpper(string, separeted = " ") {
-  return string
-    .split(separeted) // 1) Разбить строку на слова - split => array
-    .map(function (element) {
-      // 2) Перебираем массив ...
-      // - найти первую букву и сделать большой => [0] toUpperCase
-      // склеить +
-      // остальные буквы сделать маленькими
-      return element[0].toUpperCase() + element.substring(1).toLowerCase();
-    }) // новый массив из правильных слов
-    .join(separeted); // 3) Массив превратить назад в строку => join('')
-}
-console.log(stringToUpper("tO be OR Not TO bE"));
+// for (let person of people) {
+//   console.log(person);
+// }
 
-const str = "tO be OR Not TO bE";
-function stringToJadenCase(str) {
-  let words = str.split(" ");
-  let stringFinal = "";
+//forEach - делает итерацию(увеличивает n и добавляет это значение к x) для каждого елемента 
+// people.forEach(function (person) {
+//   console.log(person);
+// });
 
-  for (let i = 0; i < words.length; i++) {
-    let first = words[i].substring(0, 1).toUpperCase();
-    let leftovers = words[i].substring(1).toLowerCase();
-    stringFinal += first + leftovers + " ";
-  }
-  return stringFinal;
-}
-console.log(stringToJadenCase(str));
+// people.forEach(person =>  console.log(person));
 
-function splitString(string, separeted) {
-  const arrayOfString = string.split(separeted);
-  for (let i = 0; i < arrayOfString.length; i++) {
-    arrayOfString[i] =
-      arrayOfString[i].substring(0, 1).toUpperCase() +
-      arrayOfString[i].substring(1).toLowerCase();
-  }
-  return arrayOfString.join(separeted);
-}
-const tempestString = "sadsadsa dsadas das dss adsa sa sa sad asd";
-const space = " ";
-console.log(splitString(tempestString, space));
+// Map - схож с методом forEach, создает масив и заносит в новую переменную
+// const newPeople = people.map(person => person.age * 3);
+// console.log(newPeople);
+
+//Filter - филтрует по какому-либо условию исходный массив
+// const adults = [];
+// for (let i = 0; i < people.length; i++) {
+//   if (people[i].age >= 18) {
+//     adults.push(people[i]);
+//   }
+// }
+// console.log(adults);
+
+// const adults = people.filter((person) => person.age >= 18);
+// console.log(adults);
+
+//Reduce - служит для получения финального значения совершив итерацию по какому-то массиву
+// let amount = 0;
+// for (let i = 0; i < people.length; i++) {
+//   amount += people[i].budget;
+// }
+// console.log(amount);
+
+// const amount = people.reduce((total, person) => total + person.budget, 0)
+// console.log(amount);
+
+//Find - поиск элементов по значению
+const igor = people.find((person) => person.name === "Igor");
+console.log(igor);
+
+//FindIndex - поиск элементов по индексу
+const igorIndex = people.findIndex((person) => person.name === "Igor");
+console.log(igorIndex);
+
+// ====================================
+
+const amount = people
+  .filter((person) => person.budget > 3000)
+  .map((person) => {
+    return {
+      info: `${person.name} (${person.age})`,
+      budget: Math.sqrt(person.budget),
+    };
+  })
+  .reduce((total, person) => total + person.budget, 0);
+
+console.log(amount);
