ca5e0ccb37c69a841e893d0fc86f5b0fbacf3edf
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-08-15 13:10:00 +0300

    Add User/Moderator/Squirrel

--- index.js
+++ index.js
@@ -5,12 +5,11 @@
 - полифомирзм
 */
 class User {
-  #name;
-  #sname;
   constructor(name, sname, age) {
-    this.#name = name;
-    this.#sname = sname;
+    this._name = name;
+    this._sname = sname;
     this.age = age; // setter
+    this.isBan = false;
   }
   get age() {
     return this._age;
@@ -25,7 +24,7 @@ class User {
     this._age = age;
   }
   getfullName() {
-    return `${this.#name} ${this.#sname}`;
+    return `${this._name} ${this._sname}`;
   }
 }
 class Moderator extends User {
@@ -37,8 +36,92 @@ class Moderator extends User {
     return `${this.getfullName()} delete massege ${id}`;
   }
   sayName() {
-    return `${this.#name}`;
+    return `${this._name}`;
   }
 }
-const mod1 = new Moderator("Elon", "Musk", 50, { rule: 1 });
-console.log(mod1.deleteMessage(2));
+class Admin extends Moderator {
+  constructor(name, sname, age, rights, prop) {
+    super(name, sname, age, rights);
+    this.prop = prop;
+  }
+  // ban(user) {
+  // 	if(user instanceof User) {
+  // 		user.ifBan = true;
+  // 	}
+  //   throw new TypeError('not user!!!')
+  // }
+  // unBan(user) {
+  //   if(user instanceof User) {
+  // 		user.ifBan = false;
+  // 	}
+  //   throw new TypeError('not user!!!')
+  // }
+  toggleBan(user) {
+    if (user instanceof User) {
+      return (user.ifBan = !user.ifBan);
+    }
+    throw new TypeError("not user!!!");
+  }
+}
+const user = new User("Rob", "Bob", 24);
+const admin = new Admin("Elon", "Musk", 50, { rule: 5 }, 789);
+console.log(admin.getfullName());
+console.log(admin.toggleBan(user));
+
+// const mod1 = new Moderator("Elon", "Musk", 50, { rule: 1 });
+// console.log(mod1.deleteMessage(2));
+// console.log(mod1.sayName());
+
+class Squirrel {
+  constructor(name, color, jump) {
+    this.name = name;
+    this.color = color;
+    this.jump = jump;
+  }
+  getNameWhoJumping() {
+    return `${this.name} is jumping`;
+  }
+}
+class FlySquirrel extends Squirrel {
+  constructor(name, color, jump, distance, fly) {
+    super(name, color, jump);
+    this.distance = distance;
+    this.fly = fly;
+  }
+  getNameWhoFlying() {
+    return `${this.name} is flying`;
+  }
+}
+class MagicSquirrel extends FlySquirrel {
+  constructor(name, color, jump, distance, fly, songs, dance, sing) {
+    super(name, color, jump, distance, fly);
+    this.songs = songs;
+    this.dance = dance;
+    this.sing = sing;
+  }
+  getNameWhoDancing() {
+    return `${this.name} is dancing`;
+  }
+  getNameWhoSing() {
+    return `songs: ${this.songs.join(", ")}.`;
+  }
+}
+const squirrel = new Squirrel("Sarra", "Orange", "Yes");
+const flySquirrel = new FlySquirrel("Mishal", "Green", "Yes", 500, "Yes");
+const magicSquirrel = new MagicSquirrel(
+  "Luna",
+  "Yellow",
+  "Yes",
+  500,
+  "Yes",
+  ["p1", "p2", "p3"],
+  "Yes"
+);
+console.log(squirrel.getNameWhoJumping());
+console.log(flySquirrel.getNameWhoFlying());
+
+console.log(magicSquirrel.getNameWhoJumping());
+console.log(magicSquirrel.getNameWhoFlying());
+console.log(magicSquirrel.getNameWhoDancing());
+console.log(magicSquirrel);
+console.log(magicSquirrel.getNameWhoSing());
