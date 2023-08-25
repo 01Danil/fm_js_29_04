dd6f0a9b4bcd479a4f0c57c38c3bc3fdbd00f834
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-08-25 18:50:09 +0300

    Example Symbol, Symbol.Iterator

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 "use strict";
 
-const arr = [1,,1,[2,2,,2,[3,3,,3,[4,[5,5,,5,5],4,4,4,4],3],2,2],1]; 
+const symbol1 = Symbol("metka for dev"); // 42343rewfewds342
+const symbol2 = Symbol("metka for dev");
+
+const obj = {
+  1: 10,
+  prop1: 12,
+  symbol1: 77,
+  [symbol1]: "qwerty",
+};
+
+// delete obj.prop1;
+// delete obj[symbol1];
