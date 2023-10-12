22dd6d85592f25acff29ccb5f9af2ea59c9dd708
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-10-12 14:29:21 +0300

    Add LinkedListIterator

--- index.js
+++ index.js
@@ -1,24 +1,2 @@
 "use strict";
 
-const symbol1 = Symbol("metka for dev");
-const symbol2 = Symbol("metka for dev");
-
-const obj = {
-  1: 10,
-  prop1: 12,
-  symbol1: 77,
-  [symbol1]: "qwerty",
-};
-
-const arr = [1, 2, 3, 4];
-// for (const iterator of arr) {
-//   console.log(iterator);
-// }
-// console.log(...arr);
-
-
-const arrMy = new MyArray(1,2,3,4);
-// for (const iterator of arrMy) {
-//   console.log(iterator);
-// }
-// console.log(...arr);
