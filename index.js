71fabba46e7d4931a1506970d565622464f93605
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2023-11-15 18:06:50 +0200

    Add checkSequence

--- index.js
+++ index.js
@@ -1,2 +1,66 @@
 "use strict";
 
+// const checkSequence = (str) => {
+//   let amount1 = 0;
+//   let amount2 = 0;
+//   for (const symbol of str) {
+//     if (symbol === "(") {
+//       amount1++;
+//     }
+//     if (symbol === ")") {
+//       amount2++;
+//     }
+//   }
+//   return amount1 === amount2;
+// };
+
+// const checkSequence = (str) => {
+// 	const stack = new Stack();
+// 	for (const symbol of str) {
+// 		if(symbol === '(') {
+// 			stack.push(symbol);
+// 		}
+// 		if(stack.isEmpty){
+// 			return false;
+// 		}
+// 		if(symbol === ')') {
+// 			stack.pop();
+// 		}
+// 	}
+// 	return stack.isEmpty;
+// }
+
+// console.log(checkSequence('()()'));
+
+const options = {
+  brackets: {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  },
+};
+
+const checkSequence = (str, options) => {
+  const brackets = options.brackets;
+  const stack = new Stack();
+  for (const symbol of str) {
+    if (brackets[symbol] !== undefined) {
+      stack.push(symbol);
+      continue;
+    }
+    // if (stack.isEmpty) {
+    //   return false;
+    // }
+    const lastOfStack = stack.pick();
+		const correctSymbol = brackets[lastOfStack]
+    if (symbol === correctSymbol) {
+      stack.pop();
+    } else { 
+      return false;
+    }
+  }
+  return stack.isEmpty;
+};
+
+console.log(checkSequence("({}[])", options));
+console.log(checkSequence("(2+5)", options));
