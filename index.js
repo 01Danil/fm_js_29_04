0c32f1d2c4d0596f319a9a9f26aeaf79598c0040
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2024-02-01 15:42:49 +0200

    Add compare v1.0

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
 "use strict";
 
-const user11 = {
-	id: 11,
-  name: "Slon",
-  age: 50,
-};
-const user1 = {
-  id: 1,
-  name: "Elon",
-  age: 50,
-};
-const user2 = {
-  id: 2,
-  name: "Elen",
-  age: 40,
-};
-
-const msgElon = ["has", "already", "been", "declared"];
-const msgElen = ["Uncaught", "SyntaxError", "Unexpected", "Hi!"];
-
-const usersMessage = new Map();
-usersMessage.set(user1, msgElon);
-usersMessage.set(user2, msgElen);
-
-function getMsgs(user) {
-  if (usersMessage.has(user)) {
-    return usersMessage.get(user).forEach((msg) => {
-      console.log(msg);
-      return;
-    });
+function compare(str1, str2) {
+  const mapStr1 = new Map();
+  for (const letter of str1) {
+    if (mapStr1.has(letter)) {
+      const value = mapStr1.get(letter);
+      mapStr1.set(letter, value + 1);
+    } else {
+      mapStr1.set(letter, 1);
+    }
   }
-  console.log("404");
+  const mapStr2 = new Map();
+  for (const letter of str2) {
+    if (mapStr2.has(letter)) {
+      const value = mapStr2.get(letter);
+      mapStr2.set(letter, value + 1);
+    } else {
+      mapStr2.set(letter, 1);
+    }
+  }
+  console.log(mapStr1);
+  console.log(mapStr2);
+  if (mapStr1.size !== mapStr2.size) {
+    return false;
+  }
+  for (const letter of mapStr1.keys()) {
+    if (mapStr1.get(letter) !== mapStr2.get(letter)) {
+      return false;
+    }
+  }
+	return true;
 }
 
-for (const key of usersMessage.keys()) {
-  console.log(key.name, "says:");
-  getMsgs(key);
-}
+console.log(compare("test", "sett"));
+console.log(compare("love", "move"));
+console.log(compare("qwerty", "qwer"));
